1b7aa986681b7043c9513bad27ab40ddef9fbeae
NingZhang <ningzhang1024@gmail.com>
2024-10-04 12:43:10 -0700

    compatible with GDF

--- pyscf/isdf/isdf_to_df.py
+++ pyscf/isdf/isdf_to_df.py
@@ -19,12 +19,14 @@
 import numpy
 import contextlib
 
+from pyscf.pbc.df.df import GDF
 from pyscf.df.df import DF
 from pyscf.isdf.isdf import ISDF
 from pyscf.isdf.isdf_local import ISDF_Local
 from pyscf.lib import logger
 from pyscf.isdf.isdf_to_df_incore import cholesky_eri
 import pyscf.isdf.BackEnd.isdf_backend as BACKEND
+from pyscf import lib
 
 ToNUMPY = BACKEND._toNumpy
 
@@ -52,6 +54,7 @@ class DF_ISDF(DF):
                 self,
                 "robust fitting is used with possibly small c, DF_ISDF use only the W matrix, the error may be too large",
             )
+        self.kpts = isdf_obj.kpts
 
     def build(self):
         self._cderi = cholesky_eri(self)
@@ -66,3 +69,103 @@ class DF_ISDF(DF):
     @contextlib.contextmanager
     def range_coulomb(self, omega):
         raise NotImplementedError
+
+class GDF_ISDF(GDF):
+    r"""
+    the density fitting object adapted from (local) ISDF without robust fitting
+    """
+
+    def __init__(self, isdf_obj: ISDF):
+        cell = isdf_obj.cell
+        super().__init__(cell)
+        self._isdf_obj = isdf_obj
+        self._aoRg = isdf_obj.aoRg_full().aoR
+        # self._aoRg = ToNUMPY(self._aoRg)
+        # add a common factor #
+        factor = (isdf_obj.ngrids / isdf_obj.cell.vol) ** 0.25
+        # print("factor = ", factor)
+        self._aoRg *= factor
+        # print some warning #
+        if hasattr(isdf_obj, "outcore") and isdf_obj.outcore:
+            raise NotImplementedError("outcore is not supported")
+        if isdf_obj.with_robust_fitting:
+            logger.warning(
+                self,
+                "robust fitting is used with possibly small c, DF_ISDF use only the W matrix, the error may be too large",
+            )
+        self.kpts = isdf_obj.kpts
+
+    def get_pp(self, kpts=None):
+        return self._isdf_obj.get_pp(kpts)
+
+    def build(self, j_only=None, with_j3c=True, kpts_band=None):
+        self._cderi = cholesky_eri(self)
+        self._cderi = self._cderi.reshape(1, *self._cderi.shape)
+        self._aoRg = ToNUMPY(
+            self._aoRg
+        )  # NOTE all the involved tensors are converted to numpy array, to be consistent with pyscf
+        return self
+
+    def reset(self, mol=None):
+        raise NotImplementedError
+
+    @contextlib.contextmanager
+    def range_coulomb(self, omega):
+        raise NotImplementedError
+    
+    def sr_loop(self, kpti_kptj=numpy.zeros((2,3)), max_memory=2000,
+                compact=True, blksize=None, aux_slice=None):
+        assert numpy.allclose(kpti_kptj, numpy.zeros((2,3)))
+        # assert compact
+        cell = self.cell
+        nao = cell.nao_nr()
+        if cell.dimension == 2 and cell.low_dim_ft_type != 'inf_vacuum':
+            raise RuntimeError('ERIs of PBC-2D systems are not positive '
+                               'definite. Current API only supports positive '
+                               'definite ERIs.')
+        if blksize is None:
+            blksize = self.blockdim
+
+        if compact:
+            for LpqR in self.loop(blksize):
+                yield LpqR, numpy.zeros_like(LpqR), 1
+        else:
+            for LpqR in self.loop(blksize):
+                naux = LpqR.shape[0]
+                LpqR = lib.unpack_tril(LpqR).reshape(naux, nao**2)
+                yield LpqR, numpy.zeros_like(LpqR), 1
+    
+    def loop(self, blksize=None):
+        if self._cderi is None:
+            self.build()
+        if blksize is None:
+            blksize = self.blockdim
+
+        from pyscf.df import addons
+        import h5py
+        from pyscf.ao2mo.outcore import _load_from_h5g
+        
+        with addons.load(self._cderi, self._dataname) as feri:
+            if isinstance(feri, numpy.ndarray):
+                naoaux = feri.shape[1]
+                for b0, b1 in self.prange(0, naoaux, blksize):
+                    yield numpy.asarray(feri[0, b0:b1], order='C')
+
+            else:
+                raise NotImplementedError
+                if isinstance(feri, h5py.Group):
+                    # starting from pyscf-1.7, DF tensor may be stored in
+                    # block format
+                    naoaux = feri['0'].shape[0]
+                    def load(aux_slice):
+                        b0, b1 = aux_slice
+                        return _load_from_h5g(feri, b0, b1)
+                else:
+                    naoaux = feri.shape[0]
+                    def load(aux_slice):
+                        b0, b1 = aux_slice
+                        return numpy.asarray(feri[b0:b1])
+
+                for dat in lib.map_with_prefetch(load, self.prange(0, naoaux, blksize)):
+                    yield dat
+                    dat = None
---
+++ pyscf/isdf/tests/09-00-CCSD.py
@@ -0,0 +1,111 @@
+# backend to test #
+
+import pyscf.isdf.BackEnd._config as config
+
+config.disable_fftw()
+# config.backend("numpy")
+# config.backend("scipy")
+config.backend("torch")
+# config.backend("torch_gpu")
+import pyscf.isdf.BackEnd.isdf_backend as BACKEND
+
+MAX = BACKEND._maximum
+ABS = BACKEND._absolute
+ToTENSOR = BACKEND._toTensor
+
+# sys and pyscf #
+
+import numpy as np
+from pyscf import lib
+
+from pyscf.lib.parameters import BOHR
+from pyscf.pbc import df
+
+# isdf util #
+
+from pyscf.isdf.isdf_tools_Tsym import _kmesh_to_Kpoints, _1e_operator_gamma2k
+from pyscf.isdf import isdf_tools_cell
+from pyscf.isdf.isdf import ISDF
+from pyscf.isdf.isdf_local import ISDF_Local
+from pyscf.isdf.isdf_to_df import DF_ISDF, GDF_ISDF
+
+#############################
+
+ke_cutoff = 70
+basis = "gth-dzvp"
+
+boxlen = 3.57371000
+prim_a = np.array([[boxlen, 0.0, 0.0], [0.0, boxlen, 0.0], [0.0, 0.0, boxlen]])
+atm = [
+    ["C", (0.0, 0.0, 0.0)],
+    ["C", (0.8934275, 0.8934275, 0.8934275)],
+    ["C", (1.786855, 1.786855, 0.0)],
+    ["C", (2.6802825, 2.6802825, 0.8934275)],
+    ["C", (1.786855, 0.0, 1.786855)],
+    ["C", (2.6802825, 0.8934275, 2.6802825)],
+    ["C", (0.0, 1.786855, 1.786855)],
+    ["C", (0.8934275, 2.6802825, 2.6802825)],
+]
+
+kmeshes = [
+    [1, 1, 1],
+]  # -44.20339674
+VERBOSE = 10
+
+prim_cell = isdf_tools_cell.build_supercell(
+    atm,
+    prim_a,
+    Ls=[1, 1, 1],
+    ke_cutoff=ke_cutoff,
+    basis=basis,
+    pseudo="gth-pade",
+    verbose=VERBOSE,
+)
+
+prim_group = [[0, 1], [2, 3], [4, 5], [6, 7]]
+
+prim_mesh = prim_cell.mesh
+
+for kmesh in kmeshes:
+
+    mesh = [int(k * x) for k, x in zip(kmesh, prim_mesh)]
+    print("kmesh:", kmesh, "mesh:", mesh)
+
+    kpts = prim_cell.make_kpts(kmesh)
+
+    cell, group = isdf_tools_cell.build_supercell_with_partition(
+        atm,
+        prim_a,
+        Ls=kmesh,
+        ke_cutoff=ke_cutoff,
+        partition=prim_group,
+        mesh=mesh,
+        basis=basis,
+        pseudo="gth-pade",
+        verbose=VERBOSE,
+    )
+    cell.max_memory = 200
+    print("group:", group)
+
+    isdf = ISDF_Local(
+        cell, with_robust_fitting=False, limited_memory=True, build_V_K_bunchsize=56
+    )
+    isdf.build(c=45, m=5, rela_cutoff=1e-4, group=group)
+
+    df = GDF_ISDF(isdf)
+    df.build()
+
+    # test ao2mo #
+    
+    from pyscf.pbc import scf
+
+    mf = scf.RHF(cell)
+    mf.with_df = df
+    mf.kernel()
+    
+    from pyscf.cc.dfccsd import RCCSD
+    
+    # mf.with_df = df
+    mycc = RCCSD(mf, mo_coeff=mf.mo_coeff, mo_occ=mf.mo_occ)
+    mycc.kernel()
+
